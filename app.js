c0429f0e80a71ccc44b8ab0ef213c28eebd37a03
simonasdev <simonasgildutis@gmail.com>
2016-02-07 15:07:51 +0200

    Comments

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// var Gpio = require('pigpio').Gpio,
-//     topMotor = new Gpio(18, { mode: Gpio.OUTPUT }),
-//     bottomMotor = new Gpio(23, { mode: Gpio.OUTPUT }),
-//     pulse = 2500,
-//     button = new Gpio(15, {
-//       mode: Gpio.INPUT,
-//       pullUpDown: Gpio.PUD_UP,
-//       edge: Gpio.FALLING_EDGE
-//     }),
-//     timeout = null;
-//     // ratio = 80 / 14,
-//     // layerAngle = 60,
-//     // motorAngle = ratio * layerAngle;
+var Gpio = require('pigpio').Gpio,
+    topMotor = new Gpio(18, { mode: Gpio.OUTPUT }),
+    bottomMotor = new Gpio(23, { mode: Gpio.OUTPUT }),
+    pulse = 2500,
+    button = new Gpio(15, {
+      mode: Gpio.INPUT,
+      pullUpDown: Gpio.PUD_UP,
+      edge: Gpio.FALLING_EDGE
+    }),
+    timeout = null;
+    // ratio = 80 / 14,
+    // layerAngle = 60,
+    // motorAngle = ratio * layerAngle;
 
 var express = require('express'),
     app = express();
@@ -28,7 +28,7 @@ app.listen(3000, function () {
 });
 
 
-// button.on('interrupt', handleInterrupt);
+button.on('interrupt', handleInterrupt);
 
 function handleInterrupt (level) {
   if (level !== 0 || timeout) return false;
@@ -38,34 +38,34 @@ function handleInterrupt (level) {
   return true;
 }
 
-// function initialize () {
-//   turn(bottomMotor, 2500);
-//   turn(topMotor, 1700);
-//   // nextStage().then(nextStage).then(nextStage).finally(function () {
-//   //   timeout = null;
-//   // });
-// }
+function initialize () {
+  turn(bottomMotor, 2500);
+  turn(topMotor, 1700);
+  // nextStage().then(nextStage).then(nextStage).finally(function () {
+  //   timeout = null;
+  // });
+}
 
-// function nextStage () {
-//   return new Promise(function (resolve, reject) {
-//     // turn(topMotor, 2500);
-//     // setTimeout(stop.bind(topMotor), 2800);
-//     // setTimeout(stop.bind(bottomMotor), 2800);
-//     // turn topMotor 60 deg and turn bottomMotor 60 deg
-//     // topMotor rumble
-//     // turn topMotor 60 deg
-//     // turn bottomMotor 60 deg and resolve
-//   });
-// }
+function nextStage () {
+  return new Promise(function (resolve, reject) {
+    // turn(topMotor, 2500);
+    // setTimeout(stop.bind(topMotor), 2800);
+    // setTimeout(stop.bind(bottomMotor), 2800);
+    // turn topMotor 60 deg and turn bottomMotor 60 deg
+    // topMotor rumble
+    // turn topMotor 60 deg
+    // turn bottomMotor 60 deg and resolve
+  });
+}
 
-// function stop () {
-//   return turn(this, 0);
-// }
+function stop () {
+  return turn(this, 0);
+}
 
-// function turn (motor, microseconds) {
-//   return motor.servoWrite(microseconds);
-// }
+function turn (motor, microseconds) {
+  return motor.servoWrite(microseconds);
+}
 
-// function getPulse () {
-//   return motor.getServoPulseWidth();
-// }
+function getPulse () {
+  return motor.getServoPulseWidth();
+}
