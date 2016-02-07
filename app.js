15d84f3802b449e0b8ca2e320ed5cf2837dc10f1
simonasdev <simonasgildutis@gmail.com>
2016-02-07 12:15:08 +0200

    level

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var Gpio = require('pigpio').Gpio,
     topMotor = new Gpio(18, { mode: Gpio.OUTPUT }),
-    // bottomMotor = new Gpio(23, { mode: Gpio.OUTPUT }),
+    bottomMotor = new Gpio(23, { mode: Gpio.OUTPUT }),
     pulse = 2500,
     button = new Gpio(15, {
       mode: Gpio.INPUT,
@@ -10,26 +10,27 @@ var Gpio = require('pigpio').Gpio,
     timeout = null,
     ratio = 80 / 14,
     layerAngle = 60,
-    motorDegrees = ratio * layerAngle;
+    motorAngle = ratio * layerAngle;
 
 button.on('interrupt', function (level) {
   if (level !== 0 || timeout) return;
+  console.log(level);
 
   timeout = setTimeout(initialize, 200);
 });
 
 function initialize () {
-  nextStage()
-    // .then(nextStage).then(nextStage).finally(function () {
-    //   timeout = null;
-    // });
+  nextStage().then(nextStage).then(nextStage).finally(function () {
+    timeout = null;
+  });
 }
 
 function nextStage () {
   return new Promise(function (resolve, reject) {
     // turn(topMotor, 2500);
-    turn(bottomMotor, 2500);
-    setTimeout(stop.bind(topMotor), 2800);
+    turn(bottomMotor, 1505);
+    // setTimeout(stop.bind(topMotor), 2800);
+    // setTimeout(stop.bind(bottomMotor), 2800);
     // turn topMotor 60 deg and turn bottomMotor 60 deg
     // topMotor rumble
     // turn topMotor 60 deg
