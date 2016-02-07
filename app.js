112bd3597e8a06b6214b09ea9059c43ae4895142
simonasdev <simonasgildutis@gmail.com>
2016-02-07 02:09:18 +0200

    Timeout

--- app.js
+++ app.js
@@ -5,23 +5,29 @@ var Gpio = require('pigpio').Gpio,
     mode: Gpio.INPUT,
     pullUpDown: Gpio.PUD_UP,
     edge: Gpio.FALLING_EDGE
-  });
+  }),
+  timeout = null;
 
 button.on('interrupt', function (level) {
+  if (timeout) return;
   console.log(level)
 
-  if (pulse === 2500) {
-    turn(pulse);
-    pulse = 500;
-    console.log('1: ' + pulse);
-  } else if (getPulse() === 2500) {
-    turn(pulse);
-    console.log('2: ' + pulse);
-  } else {
-    turn(0);
-    pulse = 2500;
-    console.log('3: ' + pulse);
-  }
+  timeout = setTimeout(function () {
+    timeout = null;
+
+    if (pulse === 2500) {
+      turn(pulse);
+      pulse = 500;
+      console.log('1: ' + pulse);
+    } else if (getPulse() === 2500) {
+      turn(pulse);
+      console.log('2: ' + pulse);
+    } else {
+      turn(0);
+      pulse = 2500;
+      console.log('3: ' + pulse);
+    }
+  }, 10);
 });
 
 function turn (pulse) {
