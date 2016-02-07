cc06eab9ef8e6fa352d787bf89b07ad332f70651
simonasdev <simonasgildutis@gmail.com>
2016-02-07 11:26:57 +0200

    Try out 2 motors

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
 var Gpio = require('pigpio').Gpio,
-  motor = new Gpio(18, { mode: Gpio.OUTPUT }),
-  pulse = 2500,
-  button = new Gpio(15, {
-    mode: Gpio.INPUT,
-    pullUpDown: Gpio.PUD_UP,
-    edge: Gpio.FALLING_EDGE
-  }),
-  timeout = null;
+    topMotor = new Gpio(18, { mode: Gpio.OUTPUT }),
+    bottomMotor = new Gpio(23, { mode: Gpio.OUTPUT }),
+    pulse = 2500,
+    button = new Gpio(15, {
+      mode: Gpio.INPUT,
+      pullUpDown: Gpio.PUD_UP,
+      edge: Gpio.FALLING_EDGE
+    }),
+    timeout = null,
+    ratio = 80 / 14;
 
 button.on('interrupt', function (level) {
   if (level !== 0 || timeout) return;
 
-  timeout = setTimeout(function () {
-    if (pulse === 2500) {
-      turn(pulse);
-      pulse = 500;
-      console.log('1: ' + pulse);
-    } else if (getPulse() === 2500) {
-      turn(pulse);
-      console.log('2: ' + pulse);
-    } else {
-      turn(0);
-      pulse = 2500;
-      console.log('3: ' + pulse);
-    }
-
-    timeout = null;
-  }, 200);
+  timeout = setTimeout(initialize, 200);
 });
 
-function turn (pulse) {
-  return motor.servoWrite(pulse);
+function initialize () {
+  turn(topMotor, 1500)
+  turn(bottomMotor, 1500)
+  // nextStage().then(nextStage).then(nextStage).finally(function () {
+  //   timeout = null;
+  // });
+}
+
+function nextStage () {
+  return new Promise(function (resolve, reject) {
+    // turn topMotor 60 deg and turn bottomMotor 60 deg
+    // topMotor rumble
+    // turn topMotor 60 deg
+    // turn bottomMotor 60 deg and resolve
+  });
+}
+
+function turn (motor, microseconds) {
+  return motor.servoWrite(microseconds);
 }
 
 function getPulse () {
