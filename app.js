a8dc42aa71a3e6cd12efbed4f9bc2088c83fbc8b
simonasdev <simonasgildutis@gmail.com>
2016-02-07 11:38:41 +0200

    Try

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ var Gpio = require('pigpio').Gpio,
       edge: Gpio.FALLING_EDGE
     }),
     timeout = null,
-    ratio = 80 / 14;
+    ratio = 80 / 14,
+    layerAngle = 60,
+    motorDegrees = ratio * layerAngle;
 
 button.on('interrupt', function (level) {
   if (level !== 0 || timeout) return;
@@ -17,20 +19,24 @@ button.on('interrupt', function (level) {
 });
 
 function initialize () {
-  turn(topMotor, 1500)
-  turn(bottomMotor, 1500)
-  // nextStage().then(nextStage).then(nextStage).finally(function () {
-  //   timeout = null;
-  // });
+  nextStage().then(nextStage).then(nextStage).finally(function () {
+    timeout = null;
+  });
 }
 
 function nextStage () {
   return new Promise(function (resolve, reject) {
+    turn(topMotor, 2500);
+    setTimeout(stop.bind(null, topMotor), 20);
     // turn topMotor 60 deg and turn bottomMotor 60 deg
     // topMotor rumble
     // turn topMotor 60 deg
     // turn bottomMotor 60 deg and resolve
   });
+}
+
+function stop (motor) {
+  return turn(motor, 1500);
 }
 
 function turn (motor, microseconds) {
